7084d82cb956d2f7d6ca9d3b9187e82b7aebb5e4
Ashbin <kunal.mazumdar@brillio.com>
2018-05-21 18:21:45 +0530

    patch and remove added

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { Todo } = require('./models/todo.js')
 const express = require('express')
 const bodyParser = require('body-parser')
 const { ObjectID } = require('mongodb')
-
+const _ = require('lodash')
 const port = process.env.PORT || 3000;
 
 var app = express()
@@ -45,6 +45,46 @@ app.get('/todos/:todoId', (req, res) => {
         .then(docs => {
             res.send({ todo: docs })
         })
+        .catch(err => {
+            res.status(400).send(err)
+        })
+})
+
+app.delete('/todos/:id', (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(400).send()
+    }
+
+    Todo.findByIdAndRemove(req.params.id).then(docs => {
+        res.send({ docs })
+    })
+        .catch(err => {
+            res.status(400).send(err)
+        })
+})
+
+app.patch('/todos/:id', (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(400).send()
+    }
+    var body = _.pick(req.body, ['text', 'completed'])
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime()
+        console.log('boydddd', body)
+    } else {
+        console.log('xx', body)
+        body.completed = false
+        body.completedAt = null
+    }
+
+    Todo.findByIdAndUpdate(req.params.id, {
+        $set:  body 
+    },
+        { new: true }
+    ).then(todo => {
+        res.send({ todo })
+    })
         .catch(err => {
             res.status(400).send(err)
         })
